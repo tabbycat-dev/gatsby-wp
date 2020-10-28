50ef03075ca72ba388ec423c0a7101275ef368be
tabbycat-dev <tannguyen.tabby@gmail.com>
2020-10-28 15:21:49 +1100

    init commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,53 +1,42 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import Header from './Header';
+import OverlayMenu from './OverlayMenu';
+import Hamburger from './Hamburger';
+import Footer from './Footer';
 
-import Header from "./header"
-import "./layout.css"
+import './layout.css';
+import 'bootstrap/dist/css/bootstrap-grid.css';
 
+const Primary = styled.main`
+  padding: 110px 0 0 0;
+`;
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleOverlayMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      <Hamburger handleOverlayMenu={handleOverlayMenu} />
+      <OverlayMenu menuOpen={menuOpen} callback={handleOverlayMenu} />
+      <Header />
+      <Primary id="primary" className="content-area">
+        <main id="main" className="site-main" role="main">
+          {children}
+        </main>
+      </Primary>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
